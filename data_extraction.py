089320e9c620a0897f5fc539d43eb40f9b49f972
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-05-18 02:50:20 -0400

    reusable data extractor

---
+++ data_extraction.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Part 1
+breast_cancer_df = pd.read_csv("breast-cancer.csv") # reading
+
+# Part 2
+clean_df = breast_cancer_df.dropna() # data cleaning 
+
+def get_data(columns_to_drop=[]):
+    # type: (list) -> dict[str, list]
+    """
+    Returns the testing X and y,
+    and training X and y, 
+    at 80/20 split, 
+    excluding the features listed in columns_to_drop
+    """
+    new_df = clean_df.drop(columns_to_drop, axis=1)
+
+    X = new_df.iloc[:,2:] # columns with all features
+    y = new_df.iloc[:,1] # diagnosis column
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) # split data 80/20
+
+    return {"X_train": X_train, "X_test": X_test, "y_train": y_train, "y_test": y_test}
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from data_extraction import *
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix,classification_report
+
 def main():
-    breast_cancer_df = pd.read_csv("breast-cancer.csv") # reading
-    new_df = breast_cancer_df.dropna() #data cleaning 
-    X = new_df.iloc[:,2:] # data with all features
-    y = new_df.iloc[:,1] # class / column that we want
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2) # split data 80/20
+    print("----- Part 3 -----\n")
+
+    data_all_features = get_data();
+
+
     clf = DecisionTreeClassifier() 
-    clf.fit(X_train,y_train)
-    y_pred = clf.predict(X_test)
-    print(confusion_matrix(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
+    clf.fit(data_all_features["X_train"], data_all_features["y_train"])
+    y_pred = clf.predict(data_all_features["X_test"])
+
+    print(confusion_matrix(data_all_features["y_test"], y_pred))
+    print(classification_report(data_all_features["y_test"], y_pred))
 if __name__ == '__main__': 
     main()
